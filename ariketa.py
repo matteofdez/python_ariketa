349a46dbb81e5c0b6d433c2da211e79813b4df05
matteofdez <6ikasle@gmail.com>
2026-01-16 19:24:38 +0100

    se me había olvidado definir

--- ariketa.py
+++ ariketa.py
@@ -69,6 +69,8 @@ numeros_par(n)
 
 print('Ejercicio 6: Función para invertir una cadena' \
 'Crea invertir_cadena(cadena) que devuelva la cadena invertida.')
+def invertir_cadena(cadena):
+    return cadena[::-1]
 texto = input("Introduce una cadena de texto: ")
 invertida = invertir_cadena(texto)
 print(f"La cadena invertida es: {invertida}")
